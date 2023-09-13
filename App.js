fb6d57e3883141d0c1d2f89b9f03c0476eb2d2d3
pekkao <pekka.ojala@oamk.fi>
2023-09-13 16:12:39 +0300

    Basic version

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useCallback, useEffect } from 'react';
+import { GiftedChat } from 'react-native-gifted-chat';
+
+const replies = [
+  'How are you?',
+  'Nice to chat with you',
+  'That is impressive',
+  'Excuse me'
+];
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [messages, setMessages] = useState([]);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: '?'
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+    let newMessage = 
+    [
+      {
+        _id: messages.length + 1,
+        text: replies[Math.floor(Math.random() * replies.length)],
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: '?',
+        },
+      },
+    ]
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessage))
+  }, [])
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+  )
+}
